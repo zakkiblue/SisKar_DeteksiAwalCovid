31350208fc58a13e6c4c9d0d76a4be78c1431c58
Zakki Mahatir <zakkimahatir77@gmail.com>
2021-01-05 22:05:44 +0700

    perbaikan nilai bobot

--- SisKar.sql
+++ SisKar.sql
@@ -15,19 +15,18 @@ INSERT INTO ds_evidences(code,name)
 VALUES
 ('G1','Demam lebih dari 37 dearajat celcius'),
 ('G2','Batuk Kering'),
-('G3','Kelelahan'),
-('G4','Rasa tidak nyaman dan Nyeri'),
-('G5','Nyeri tenggorokan'),
-('G6','Diare'),
-('G7','Mata Merah'),
-('G8','Sakit kepala'),
-('G9','Hilangnya indra perasa atau pencium'),
-('G10','Ruam pada kulit dan perubahan warna pada jari tangan atau kaki'),
-('G11','Sesak nafas'),
-('G12','Nyeri atau rasa tertekan pada dada'),
-('G13','HIlangnya kemampuan bicara atau bergerak'),
-('G14','Kontak dengan pasien Covid-19'),
-('G15','Bepergian ke luar kota dengan zona merah'); 
+('G3','Hidung meler'),
+('G4','Pernapasan Cepat tidak normal'),
+('G5','Nersin bersin'),
+('G6','Munath muntah'),
+('G7','Diare'),
+('G8','Dahak kental kuning kehijauan'),
+('G9','Otot -otot  nyeri'),
+('G10','Sinar X pada paru'),
+('G11','Sakit kepala'),
+('G12','Hidung tersumbat'),
+('G13','Tenggorokan sakit'),
+('G14','Tenggorokan tidak nyaman'); 
 
 DROP TABLE IF EXISTS ds_problems;
 CREATE TABLE IF NOT EXISTS ds_problems(
@@ -41,7 +40,7 @@ CREATE TABLE IF NOT EXISTS ds_problems(
 INSERT INTO ds_problems(code,name,notes)
 VALUES
 ('P1','Covid-19','anda teridentifikasi mengalami gejala Covid-19 perlu pengetesan secara mendalam di rumah sakit terdekat.'),
-('P2','Masuk Angin','Gejala yang anda alami adalah masuka anain.'),
+('P2','Influenza','Gejala yang anda alami adalah Influenza.'),
 ('P3','Flu','Anda mengalami gejala Flu biasa.');
 
 DROP TABLE IF EXISTS ds_rules;
@@ -54,32 +53,20 @@ CREATE TABLE IF NOT EXISTS ds_rules(
 INSERT INTO ds_rules(id_problem,id_evidence,cf)
 VALUES
 ('1','1','0.3'),
-('2','1','0.3'),
-('3','1','0.3'),
-('1','2','0.3'),
+('2','1','0.2'),
+('1','2','0.4'),
 ('2','2','0.3'),
 ('3','2','0.3'),
-('1','3','0.3'),
-('2','3','0.3'),
-('3','3','0.3'),
-('1','4','0.6'),
-('2','4','0.6'),
-('1','5','0.6'),
-('2','5','0.6'),
-('3','5','0.6'),
-('1','6','0.6'),
-('2','6','0.6'),
-('1','7','0.6'),
-('2','7','0.6'),
-('1','8','0.6'),
-('2','8','0.6'),
-('3','8','0.6'),
-('1','9','0.6'),
-('3','9','0.6'),
-('1','10','0.7'),
-('1','11','0.6'),
-('3','11','0.6'),
-('1','12','0.6'),
-('1','13','0.7'),
-('1','14','0.85'),
-('1','15','0.8'); 
+('3','3','0.4'),
+('1','4','0.5'),
+('2','5','0.5'),
+('3','5','0.4'),
+('2','6','0.2'),
+('1','7','0.5'),
+('1','8','0.2'),
+('2','9','0.2'),
+('1','10','0.5'),
+('2','11','0.3'),
+('3','12','0.4'),
+('3','13','0.2'),
+('3','14','0.2'); 
